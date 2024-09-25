b999bc696fb0143724f11521a34e5b40b300e3d2
Pooja Ajithan <poojaajithan91@gmail.com>
2024-09-25 23:43:11 +0530

    Initial commit for video stream app -  entities

---
+++ src/main/java/com/stream/app/spring_stream_backend/entities/Course.java
@@ -0,0 +1,19 @@
+package com.stream.app.spring_stream_backend.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "yt_courses")
+public class Course {
+	@Id
+	private String id;
+	private String title;
+	/*@OneToMany(mappedBy = "course")
+	private List<Video> list = new ArrayList<>();*/
+}
---
+++ src/main/java/com/stream/app/spring_stream_backend/entities/Video.java
@@ -0,0 +1,29 @@
+package com.stream.app.spring_stream_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "yt_videos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Video {
+	@Id
+	private String videoId;
+	private String title;
+	private String description;
+	private String contentType;
+	private String path;
+	/*@ManyToOne
+	private Course course;*/
+}
